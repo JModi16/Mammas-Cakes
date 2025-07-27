3ee2fb981715112367b361a0a3d266a1d5b8646a
JModi16 <k5rac70j@students.codeinstitute.net>
2025-07-28 00:09:15 +0100

    updated settings.py to recognize templates directory

--- mammas-cakes-1/mammas_cakes/settings.py
+++ mammas-cakes-1/mammas_cakes/settings.py
@@ -48,7 +48,10 @@ ROOT_URLCONF = 'mammas_cakes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'cakes/templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR.parent, ''),  # Look in parent directory (Mammas-Cakes/)
+            os.path.join(BASE_DIR, 'templates'),  # Also look in project templates if you create any
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -56,6 +59,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
