9a0a9a3c9c3031e686aa77e05af89c00f51495b6
JModi16 <k5rac70j@students.codeinstitute.net>
2025-08-01 23:31:52 +0100

    edited views.py

--- cakes/views.py
+++ cakes/views.py
@@ -1,4 +1,4 @@
-# filepath: c:\Users\jits_\Mammas-Cakes\cakes\views.py
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
