f3e2d5f265cbfe907caa45e141b550920e3ecd14
Your Name <your-email@example.com>
2025-08-12 10:37:18 +0100

    added authentication URLs to urls.py

--- cakes/urls.py
+++ cakes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -11,4 +12,9 @@ urlpatterns = [
     path('treats/', views.treats, name='treats'),
     path('all-cakes-treats/', views.all_cakes_treats, name='all_cakes_treats'),
     path('products/', views.products, name='products'),
+
+     # Authentication URLs
+    path('accounts/register/', views.register_view, name='register'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
