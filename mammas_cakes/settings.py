2ce08d7c70ca90cb9fc60c1417118dd6933d944b
Your Name <your-email@example.com>
2025-08-16 15:40:22 +0100

    commented out postgresql in settings.py and corrected env.py

--- mammas_cakes/settings.py
+++ mammas_cakes/settings.py
@@ -74,24 +74,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mammas_cakes.wsgi.application'
 
-#Database - SECURE VERSION
-#Database configuration
-#DATABASE_URL = os.environ.get('DATABASE_URL')
 
-# Replace the PostgreSQL database config with simple SQLite:
- #DATABASES = {
-# 'default': {
-# ENGINE': 'django.db.backends.sqlite3',
-# 'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
-#Comment out the PostgreSQL config for now:
-#if 'DATABASE_URL' in os.environ:
-#DATABASES['default'] = dj_database_url.parse(os.environ.get('DATABASE_URL'))
-
-#Password validation
-#https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 DATABASES = {
         'default': {
