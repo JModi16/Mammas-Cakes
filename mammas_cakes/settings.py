6a7c8ea17f01db52ca42bf0699d6c2c15a7499e4
Your Name <your-email@example.com>
2025-08-19 16:02:38 +0100

    adjusted settings.py for PEP8 linter

--- mammas_cakes/settings.py
+++ mammas_cakes/settings.py
@@ -142,14 +142,22 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 # Email settings - Use environment variables
-EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND',
-                               'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_BACKEND = os.environ.get(
+    'EMAIL_BACKEND',
+    'django.core.mail.backends.smtp.EmailBackend'
+)
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = f"Mamma's Cakes <{os.environ.get('EMAIL_HOST_USER', '')}>"
+EMAIL_HOST_USER = os.environ.get(
+    'EMAIL_HOST_USER', ''
+)
+EMAIL_HOST_PASSWORD = os.environ.get(
+    'EMAIL_HOST_PASSWORD', ''
+)
+DEFAULT_FROM_EMAIL = (
+    f"Mamma's Cakes <{os.environ.get('EMAIL_HOST_USER', '')}>"
+)
 
 # Heroku settings
 
